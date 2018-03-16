45d84f69bd8fb70e6fe40383c929a2bf0685971f
sannankhalid <sannankhalid@hotmail.com>
2018-03-17 00:12:26 +0500

    user fileter

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -1,4 +1,5 @@
 class IdentitiesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_identity, only: [:show, :edit, :update, :destroy]
 
   # GET /identities
